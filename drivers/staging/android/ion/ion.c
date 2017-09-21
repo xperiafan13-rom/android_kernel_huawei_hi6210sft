f8a13568bfc6489c951060f861b3ea50b9ef6f9a
xperiafan13-rom <androidanimerom@gmail.com>
2017-09-20 19:52:48 -0500

    UPSTREAM staging: ion: Fix error handling in ion_buffer_create This patch fixes error handling case when buffer->pages allocation fails. Also, it removes unreachable code of checking ret variable although it is not updated.
    Signed-off-by: Rohit kumar <rohit.kr@samsung.com>
    Reviewed-by: Laura Abbott <labbott@redhat.com>
    Suggested-by: Pintu Kumar <pintu.k@samsung.com>
    Reviewed-by: Pintu Kumar <pintu.k@samsung.com>
    Reviewed-by: Gioh Kim <gioh.kim@lge.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit a56d092aa94ebcc9452ddaa47423b9a478aa6aa5)
    Change-Id: Ic38b8e3ef0a21de4e38e58b4bb942535fe671ae5
    Bug: 34283718

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -235,10 +235,8 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 		pr_err("%s: map dma failed!\n", __func__);
 	}
 	if (IS_ERR(table)) {
-		heap->ops->free(buffer);
-		kfree(buffer);
-		pr_err("%s: map dma error!\n", __func__);
-		return ERR_PTR(PTR_ERR(table));
+		ret = -EINVAL;
+		goto err1;
 	}
 	buffer->sg_table = table;
 	if (ion_buffer_fault_user_mappings(buffer)) {
@@ -249,7 +247,7 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 		buffer->pages = vmalloc(sizeof(struct page *) * num_pages);
 		if (!buffer->pages) {
 			ret = -ENOMEM;
-			goto err1;
+			goto err;
 		}
 
 		for_each_sg(table->sgl, sg, table->nents, i) {
@@ -259,8 +257,6 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 				buffer->pages[k++] = page++;
 		}
 
-		if (ret)
-			goto err;
 	}
 
 	buffer->dev = dev;
@@ -313,10 +309,8 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 
 err:
 	heap->ops->unmap_dma(heap, buffer);
-	heap->ops->free(buffer);
 err1:
-	if (buffer->pages)
-		vfree(buffer->pages);
+	heap->ops->free(buffer);
 err2:
 	kfree(buffer);
 
